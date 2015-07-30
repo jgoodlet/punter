7ffe6f15053160c648ab0b93577dc03f09548d79
Joshua Goodlett <joshuagoodlett@gmail.com>
2015-07-30 12:41:16 -0400

    Added some tests.

--- tests/test_punter.py
+++ tests/test_punter.py
@@ -1,4 +1,31 @@
-from punter import api
+"""Test suite for the punter library."""
 
-def test_search():
-    assert(True == True)
+
+from punter.api import search
+from punter.helpers import get_query_type, get_endpoint
+
+
+class TestQueryType:
+
+    def test_query_type_email_valid(self):
+        emails = [
+            'johndoe@gmail.com',
+            'john.doe@gmail.com',
+            'john-doe007@gmail.com',
+            'john+doe007@gmail.com',
+        ]
+
+        for email in emails:
+            assert get_query_type(email) == 'email'
+
+    def test_query_type_email_malformed(self):
+        emails = [
+            'johndoe @ gmail.com',
+            'john.doe@gmail',
+            'john.doe@.com',
+            'john@'
+            '@', ''
+        ]
+
+        for email in emails:
+            assert get_query_type(email) == ''
